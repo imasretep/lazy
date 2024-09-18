a1af4a3f9e77dd25b6b3f0a512fb256a0bf15668
imasretep <samipetersalmi@gmail.com>
2024-09-18 23:07:20 +0300

    Theme Everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -16,14 +16,14 @@ return {
   -- },
   { "folke/tokyonight.nvim" },
   { "rose-pine/neovim", name = "rose-pine" },
-  {
-    "AlexvZyl/nordic.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("nordic").load()
-    end,
-  },
+  -- {
+  --   "AlexvZyl/nordic.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("nordic").load()
+  --   end,
+  -- },
   { "Shatur/neovim-ayu" },
   { "marko-cerovac/material.nvim" },
   { "catppuccin/nvim", lazy = true },
@@ -31,11 +31,24 @@ return {
   { "rmehri01/onenord.nvim" },
   { "EdenEast/nightfox.nvim" },
 
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000,
+
+    config = function()
+      require("everforest").setup({
+        -- Config here
+      })
+    end,
+  },
+
   -- Load theme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nordic",
+      colorscheme = "everforest",
     },
   },
 }
