611d67ab830845643288db0108fa6667516924b9
imasretep <samipetersalmi@gmail.com>
2024-09-27 20:29:19 +0300

    Cleaning

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 💤 LazyVim settings
 
-## Theme: Tokyonight-moon
+## Theme: Everforest
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -15,24 +15,14 @@ return {
       })
     end,
   },
-  {
-    "Shatur/neovim-ayu",
-
-    config = function()
-      require("ayu").setup({
-        mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
-        terminal = true, -- Set to `false` to let terminal manage its own colors.
-        overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
-      })
-    end,
-  },
   { "sainnhe/gruvbox-material" },
+  { "rmehri01/onenord.nvim" },
 
   -- Load theme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "everforest",
     },
   },
 }
